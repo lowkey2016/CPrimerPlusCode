f02d264b28ca00f2d84814698f8243040ea1060b
chenjunyu <chenjunyu@youmi.net>
2016-01-19 22:54:33 +0800

    Initial Commit

---
+++ CPrimerPlusCode/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  CPrimerPlusCode
+//
+//  Created by Jymn_Chen on 16/1/19.
+//  Copyright © 2016年 com.timedancing. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
